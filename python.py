fa80bab6436f72b810cc3ca186072feeee7d396f
ElTortas35 <168780812+ElTortas35@users.noreply.github.com>
2024-05-02 15:41:00 -0600

    Create python.py

---
+++ python.py
@@ -0,0 +1,21 @@
+meme_dict = {
+            "PANA": "Se refiere a amigo",
+            "XD": "Una expresion de risa",
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            }
+print(meme_dict)
+print("No entiendes una palabra buscula en el buscador con mayusculas")
+word = input("Palabra:")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    Y = input("Otra palabra que no entiendas Si/No:")
+    
+if Y=="Si" or Y=="si":
+    word2 = input("Palabra:")
+    
+if word2 in meme_dict.keys():
+    print(meme_dict[word2])
+else:
+    print("Palabra no encontrado")
